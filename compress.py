9e3c11572692730a96640d7c5e9dfc23c0a310ee
lachlan.stuart <26366936+LachlanStuart@users.noreply.github.com>
2020-12-27 12:47:13 +0100

    Format code with Black

--- compress.py
+++ compress.py
@@ -1,174 +1,191 @@
 #!/usr/bin/env python3
 import argparse
 
-def compress_rle(data):
-  last_datum = None
-  count = 0
-  for datum in data:
-    if datum == 0x81:
-      yield 0x81
-      yield 0x00
-      # last_datum = None
-      # count = 0
-    elif datum == last_datum and count < 255:
-      count += 1
-    elif count >= 2:
-      yield 0x81
-      yield count + 1
-      count = 0
-      last_datum = datum
-      yield datum
-    else:
-      for i in range(count):
-        yield last_datum
-      count = 0
-      last_datum = datum
-      yield datum
 
-  if count >= 2:
-    yield 0x81
-    yield count + 1
-  else:
-    for i in range(count):
-      yield last_datum
+def compress_rle(data):
+    last_datum = None
+    count = 0
+    for datum in data:
+        if datum == 0x81:
+            yield 0x81
+            yield 0x00
+            # last_datum = None
+            # count = 0
+        elif datum == last_datum and count < 255:
+            count += 1
+        elif count >= 2:
+            yield 0x81
+            yield count + 1
+            count = 0
+            last_datum = datum
+            yield datum
+        else:
+            for i in range(count):
+                yield last_datum
+            count = 0
+            last_datum = datum
+            yield datum
+
+    if count >= 2:
+        yield 0x81
+        yield count + 1
+    else:
+        for i in range(count):
+            yield last_datum
+
 
 def compress_lzw(data):
-  dictionary = []
-  # init
-  prev = data[0:1]
-  prev_cw = 0
-  yield 0x101, 0x100, b''
-  yield 0x101, prev[0], prev
-  data = data[1:]
-  while len(data) > 0:
-    # Brute-force search for the longest dictionary entry that prefixes data
-    longest_i = 0
-    longest_len = 0
-    for i, string in enumerate(dictionary):
-      if data.startswith(string) and len(string) > longest_len:
-        longest_i = i
-        longest_len = len(string)
+    dictionary = []
+    # init
+    prev = data[0:1]
+    prev_cw = 0
+    yield 0x101, 0x100, b""
+    yield 0x101, prev[0], prev
+    data = data[1:]
+    while len(data) > 0:
+        # Brute-force search for the longest dictionary entry that prefixes data
+        longest_i = 0
+        longest_len = 0
+        for i, string in enumerate(dictionary):
+            if data.startswith(string) and len(string) > longest_len:
+                longest_i = i
+                longest_len = len(string)
 
-    # The decompressor supports a special case when cw == cw_next it repeats the
-    # last cw's string, then repeats the first byte of that string.
-    if prev_cw >= 0x102:
-      special = dictionary[prev_cw - 0x102]
-      special = special + special[:1]
-    else:
-      special = None
+        # The decompressor supports a special case when cw == cw_next it repeats the
+        # last cw's string, then repeats the first byte of that string.
+        if prev_cw >= 0x102:
+            special = dictionary[prev_cw - 0x102]
+            special = special + special[:1]
+        else:
+            special = None
 
-    if special is not None and len(special) >= longest_len and data.startswith(special):
-      # The special case was better than a dictionary match, use it
-      string = special
-      cw = 0x102 + len(dictionary)
-    elif longest_len:
-      # Found a good dictionary match
-      string = dictionary[longest_i]
-      cw = 0x102 + longest_i
-    else:
-      # No dictionary match - emit the raw value
-      string = data[0:1]
-      cw = string[0]
-    yield len(dictionary) + 0x102, cw, string
-    dictionary.append(prev + string[0:1])
-    prev = string
-    prev_cw = cw
-    data = data[len(string):]
+        if (
+            special is not None
+            and len(special) >= longest_len
+            and data.startswith(special)
+        ):
+            # The special case was better than a dictionary match, use it
+            string = special
+            cw = 0x102 + len(dictionary)
+        elif longest_len:
+            # Found a good dictionary match
+            string = dictionary[longest_i]
+            cw = 0x102 + longest_i
+        else:
+            # No dictionary match - emit the raw value
+            string = data[0:1]
+            cw = string[0]
+        yield len(dictionary) + 0x102, cw, string
+        dictionary.append(prev + string[0:1])
+        prev = string
+        prev_cw = cw
+        data = data[len(string) :]
 
-    if len(dictionary) + 0x102 >= 0x1000:
-      # init
-      prev = data[0:1]
-      prev_cw = 0
-      yield len(dictionary) + 0x102, 0x100, b''
-      dictionary = []
-      yield 0x101, prev[0], prev
-      data = data[1:]
+        if len(dictionary) + 0x102 >= 0x1000:
+            # init
+            prev = data[0:1]
+            prev_cw = 0
+            yield len(dictionary) + 0x102, 0x100, b""
+            dictionary = []
+            yield 0x101, prev[0], prev
+            data = data[1:]
 
-  yield len(dictionary) + 0x102, 0x101, b''
+    yield len(dictionary) + 0x102, 0x101, b""
+
 
 def write_lzw_debug_file(debug_file, lzw_strings):
-  for cw_next, cw, string in lzw_strings:
-    if cw == 0x100:
-      continue
-    clean_text = ''.join(c if ' ' < c < '~' else '#' for c in string.decode("ascii", errors="replace"))
-    debug_file.write(
-      f'{cw_next: 4x}\t{cw: 4x}\t{string.hex().ljust(32)}\t{clean_text}\n'
-    )
+    for cw_next, cw, string in lzw_strings:
+        if cw == 0x100:
+            continue
+        clean_text = "".join(
+            c if " " < c < "~" else "#"
+            for c in string.decode("ascii", errors="replace")
+        )
+        debug_file.write(
+            f"{cw_next: 4x}\t{cw: 4x}\t{string.hex().ljust(32)}\t{clean_text}\n"
+        )
+
 
 def pack_lzw_cws(lzw_strings):
-  out = bytearray()
-  res = 0
-  res_bits = 0
-  for cw_next, cw, string in lzw_strings:
-    if cw_next < 0x200:
-      cw_bits = 9
-    elif cw_next < 0x400:
-      cw_bits = 10
-    elif cw_next < 0x800:
-      cw_bits = 11
-    elif cw_next < 0x1000:
-      cw_bits = 12
+    out = bytearray()
+    res = 0
+    res_bits = 0
+    for cw_next, cw, string in lzw_strings:
+        if cw_next < 0x200:
+            cw_bits = 9
+        elif cw_next < 0x400:
+            cw_bits = 10
+        elif cw_next < 0x800:
+            cw_bits = 11
+        elif cw_next < 0x1000:
+            cw_bits = 12
 
-    res |= cw << res_bits
-    res_bits += cw_bits
-    while res_bits >= 8:
-      out.append(res.to_bytes(4, 'little')[0])
-      res_bits -= 8
-      res = res >> 8
-  while res_bits >= 0:
-    out.append(res.to_bytes(4, 'little')[0])
-    res_bits -= 8
-    res = res >> 8
-  return bytes(out)
+        res |= cw << res_bits
+        res_bits += cw_bits
+        while res_bits >= 8:
+            out.append(res.to_bytes(4, "little")[0])
+            res_bits -= 8
+            res = res >> 8
+    while res_bits >= 0:
+        out.append(res.to_bytes(4, "little")[0])
+        res_bits -= 8
+        res = res >> 8
+    return bytes(out)
 
 
 def parse_path_offset(path_and_offset):
-  try:
-    path, offset = path_and_offset.rsplit(':', maxsplit=1)
-    if offset.startswith('0x'):
-      offset = int(offset[2:], base=16)
-    else:
-      offset = int(offset)
-    return path, offset
-  except:
-    return path_and_offset, None
+    try:
+        path, offset = path_and_offset.rsplit(":", maxsplit=1)
+        if offset.startswith("0x"):
+            offset = int(offset[2:], base=16)
+        else:
+            offset = int(offset)
+        return path, offset
+    except:
+        return path_and_offset, None
+
 
 def main(in_path, out_path, debug_path):
-  in_path, in_offset = parse_path_offset(in_path)
-  out_path, out_offset = parse_path_offset(out_path)
-  in_file = open(in_path, 'rb')
-  if in_offset is not None:
-    in_file.seek(in_offset)
+    in_path, in_offset = parse_path_offset(in_path)
+    out_path, out_offset = parse_path_offset(out_path)
+    in_file = open(in_path, "rb")
+    if in_offset is not None:
+        in_file.seek(in_offset)
 
-  unpacked_bytes = in_file.read()
-  print(f'Read {len(unpacked_bytes)} bytes')
-  rle_bytes = bytes(bytearray(compress_rle(unpacked_bytes)))
-  print(f'Compressed to {len(rle_bytes)} RLE bytes')
-  lzw_strings = list(compress_lzw(rle_bytes))
-  print(f'Compressed to {len(lzw_strings)} LZW codewords')
-  if debug_path is not None:
-    write_lzw_debug_file(open(debug_path, 'wt'), lzw_strings)
-    print('Wrote debug file')
-  packed_bytes = pack_lzw_cws(lzw_strings)
-  print(f'Compressed to {len(packed_bytes)} bytes')
+    unpacked_bytes = in_file.read()
+    print(f"Read {len(unpacked_bytes)} bytes")
+    rle_bytes = bytes(bytearray(compress_rle(unpacked_bytes)))
+    print(f"Compressed to {len(rle_bytes)} RLE bytes")
+    lzw_strings = list(compress_lzw(rle_bytes))
+    print(f"Compressed to {len(lzw_strings)} LZW codewords")
+    if debug_path is not None:
+        write_lzw_debug_file(open(debug_path, "wt"), lzw_strings)
+        print("Wrote debug file")
+    packed_bytes = pack_lzw_cws(lzw_strings)
+    print(f"Compressed to {len(packed_bytes)} bytes")
 
-  if out_offset is not None:
-    out_data = bytearray(open(out_path, 'rb').read())
-    out_data[out_offset:out_offset+len(packed_bytes)] = packed_bytes
-    open(out_path, 'wb').write(out_data)
-  else:
-    open(out_path, 'wb').write(packed_bytes)
+    if out_offset is not None:
+        out_data = bytearray(open(out_path, "rb").read())
+        out_data[out_offset : out_offset + len(packed_bytes)] = packed_bytes
+        open(out_path, "wb").write(out_data)
+    else:
+        open(out_path, "wb").write(packed_bytes)
 
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-    'in_path', help='Input file path, optionally suffixed by colon then a byte offset e.g. rom.sfc:0x80ff or rom.sfc:65535 (prefix with 0x for hex)'
-  )
-  parser.add_argument(
-    'out_path', help='Output file path, optionally suffixed by colon then an offset to write a range into an existing file'
-  )
-  parser.add_argument('--debug', nargs='?', help='(Optional) Path to write a debug file listing compressed strings')
-  args = parser.parse_args()
-  main(args.in_path, args.out_path, args.debug)
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "in_path",
+        help="Input file path, optionally suffixed by colon then a byte offset e.g. rom.sfc:0x80ff or rom.sfc:65535 (prefix with 0x for hex)",
+    )
+    parser.add_argument(
+        "out_path",
+        help="Output file path, optionally suffixed by colon then an offset to write a range into an existing file",
+    )
+    parser.add_argument(
+        "--debug",
+        nargs="?",
+        help="(Optional) Path to write a debug file listing compressed strings",
+    )
+    args = parser.parse_args()
+    main(args.in_path, args.out_path, args.debug)
--- decompress.py
+++ decompress.py
@@ -1,204 +1,217 @@
 #!/usr/bin/env python3
 import argparse
+
 MAX_CW_SIZE = 12
 CW_MASK = [2 ** i - 1 for i in range(16)]
 
+
 def iter_codewords(stream):
-  bits_read = 0
-  buf = 0
-  buf_len = 0
-  stream = stream
-  cw_size = 9
-  cw_next = 0x102
+    bits_read = 0
+    buf = 0
+    buf_len = 0
+    stream = stream
+    cw_size = 9
+    cw_next = 0x102
 
-  for i in range(0x4000):
-    addr = bits_read
-    bits_read += cw_size
-    while buf_len < cw_size:
-      buf = buf | int.from_bytes(stream.read(1), 'little') << buf_len
-      buf_len += 8
-    cw = buf & CW_MASK[cw_size]
-    buf = buf >> cw_size
-    buf_len -= cw_size
+    for i in range(0x4000):
+        addr = bits_read
+        bits_read += cw_size
+        while buf_len < cw_size:
+            buf = buf | int.from_bytes(stream.read(1), "little") << buf_len
+            buf_len += 8
+        cw = buf & CW_MASK[cw_size]
+        buf = buf >> cw_size
+        buf_len -= cw_size
 
-    if cw == 0x100:
-      # print(f'At {addr:04x} reinit')
-      cw_size = 9
-      cw_next = 0x101 # This is one less than 0x102 because the next cw is written directly and doesn't count
-      yield addr, None, cw
-    elif cw == 0x101:
-      return
-    else:
-      assert cw <= cw_next, (cw, cw_next)
-      yield addr, cw_next, cw
-      cw_next += 1
-
-      if cw_next >= (1 << cw_size):
-        if cw_size + 1 <= MAX_CW_SIZE:
-          # print(f'At {addr:04x} increased cw_size to {cw_size + 1} bits')
-          cw_size += 1
+        if cw == 0x100:
+            # print(f'At {addr:04x} reinit')
+            cw_size = 9
+            cw_next = 0x101  # This is one less than 0x102 because the next cw is written directly and doesn't count
+            yield addr, None, cw
+        elif cw == 0x101:
+            return
         else:
-          # print(f'At {addr:04x} cw_size got too big. Next command should be reinit.')
-          pass
-  raise Exception('Input was longer than expected (>16kB)')
+            assert cw <= cw_next, (cw, cw_next)
+            yield addr, cw_next, cw
+            cw_next += 1
+
+            if cw_next >= (1 << cw_size):
+                if cw_size + 1 <= MAX_CW_SIZE:
+                    # print(f'At {addr:04x} increased cw_size to {cw_size + 1} bits')
+                    cw_size += 1
+                else:
+                    # print(f'At {addr:04x} cw_size got too big. Next command should be reinit.')
+                    pass
+    raise Exception("Input was longer than expected (>16kB)")
 
 
 def decompress_lzw(codewords):
-  def get_dict_string(cw):
-    result = b''
-    while cw > 0xff:
-      char, cw = dictionary[cw - 0x102]
-      result = char + result
-    result = cw.to_bytes(1, 'little') + result
-    return result
+    def get_dict_string(cw):
+        result = b""
+        while cw > 0xFF:
+            char, cw = dictionary[cw - 0x102]
+            result = char + result
+        result = cw.to_bytes(1, "little") + result
+        return result
 
-  dictionary = [] # Codeword 0x0102 corresponds to self.dictionary[0]
-  pw = 0 # previous cw
-  for addr, cw_next, cw in codewords:
-    # print(f'At {addr:04x}: {cw}')
-    if pw == 0x100:
-      # Last codeword was a reset - this codeword skips the dictionary completely
-      yield addr, cw_next, cw, cw.to_bytes(1, 'little')
-    elif cw == 0x100:
-      dictionary = []
-    else:
-      if cw < cw_next:
-        # Normal case - cw is either a dictionary entry (>0x102) or a literal value
-        string = get_dict_string(cw)
-        yield addr, cw_next, cw, string
-      else:
-        # Special case - use last cw's string, then repeat the first byte.
-        # Don't think too hard about it. It saves bytes, it doesn't need to make sense.
-        # This case isn't used in some files, but it's used by the title screen data.
-        string = get_dict_string(pw)
-        yield addr, cw_next, cw, string + string[0:1]
+    dictionary = []  # Codeword 0x0102 corresponds to self.dictionary[0]
+    pw = 0  # previous cw
+    for addr, cw_next, cw in codewords:
+        # print(f'At {addr:04x}: {cw}')
+        if pw == 0x100:
+            # Last codeword was a reset - this codeword skips the dictionary completely
+            yield addr, cw_next, cw, cw.to_bytes(1, "little")
+        elif cw == 0x100:
+            dictionary = []
+        else:
+            if cw < cw_next:
+                # Normal case - cw is either a dictionary entry (>0x102) or a literal value
+                string = get_dict_string(cw)
+                yield addr, cw_next, cw, string
+            else:
+                # Special case - use last cw's string, then repeat the first byte.
+                # Don't think too hard about it. It saves bytes, it doesn't need to make sense.
+                # This case isn't used in some files, but it's used by the title screen data.
+                string = get_dict_string(pw)
+                yield addr, cw_next, cw, string + string[0:1]
 
-      dictionary.append((string[0:1], pw))
+            dictionary.append((string[0:1], pw))
 
-    pw = cw
+        pw = cw
+
 
 def extract_data_from_lzw_lines(lzw_strings):
-  return b''.join(string for addr, cw_next, cw, string in lzw_strings)
+    return b"".join(string for addr, cw_next, cw, string in lzw_strings)
+
 
 def write_lzw_debug_file(debug_file, lzw_strings):
-  for addr, cw_next, cw, string in lzw_strings:
-    clean_text = ''.join(c if ' ' < c < '~' else '#' for c in string.decode("ascii", errors="replace"))
-    debug_file.write(
-      # f'{addr: 8x}\t'
-      f'{cw_next: 4x}\t{cw: 4x}\t'
-      f'{string.hex().ljust(32)}\t{clean_text}\n'
-    )
+    for addr, cw_next, cw, string in lzw_strings:
+        clean_text = "".join(
+            c if " " < c < "~" else "#"
+            for c in string.decode("ascii", errors="replace")
+        )
+        debug_file.write(
+            # f'{addr: 8x}\t'
+            f"{cw_next: 4x}\t{cw: 4x}\t"
+            f"{string.hex().ljust(32)}\t{clean_text}\n"
+        )
+
 
 def decompress_rle(data):
-  rle_on = False
-  last_datum = b'\0'
-  result = bytearray()
-  for datum in data:
-    if rle_on:
-      rle_on = False
-      if datum == 0:
-        # print(f'Escaped 0x81')
-        result.append(0x81)
-      else:
-        # print(f'RLE! {last_datum} * {datum}')
-        for i in range(datum - 1):
-          result.append(last_datum)
-    elif datum == 0x81:
-      rle_on = True
-    else:
-      result.append(datum)
-      last_datum = datum
+    rle_on = False
+    last_datum = b"\0"
+    result = bytearray()
+    for datum in data:
+        if rle_on:
+            rle_on = False
+            if datum == 0:
+                # print(f'Escaped 0x81')
+                result.append(0x81)
+            else:
+                # print(f'RLE! {last_datum} * {datum}')
+                for i in range(datum - 1):
+                    result.append(last_datum)
+        elif datum == 0x81:
+            rle_on = True
+        else:
+            result.append(datum)
+            last_datum = datum
 
-  return bytes(result)
+    return bytes(result)
 
 
 def parse_path_offset(path_and_offset):
-  try:
-    path, offset = path_and_offset.rsplit(':', maxsplit=1)
-    if offset.startswith('0x'):
-      offset = int(offset[2:], base=16)
-    else:
-      offset = int(offset)
-    return path, offset
-  except:
-    return path_and_offset, None
+    try:
+        path, offset = path_and_offset.rsplit(":", maxsplit=1)
+        if offset.startswith("0x"):
+            offset = int(offset[2:], base=16)
+        else:
+            offset = int(offset)
+        return path, offset
+    except:
+        return path_and_offset, None
 
 
 def scan_file(in_path):
-  in_file = open(in_path, 'rb')
-  in_bytes = in_file.read()
-  potential_offsets = []
-  for off in range(0, len(in_bytes), 0x10):
-    if in_bytes[off] == 0 and in_bytes[off+1] & 1:
-      potential_offsets.append(off)
+    in_file = open(in_path, "rb")
+    in_bytes = in_file.read()
+    potential_offsets = []
+    for off in range(0, len(in_bytes), 0x10):
+        if in_bytes[off] == 0 and in_bytes[off + 1] & 1:
+            potential_offsets.append(off)
 
-  for in_offset in potential_offsets:
-    try:
-      in_file.seek(in_offset)
-      codewords = list(iter_codewords(in_file))
-      lzw_strings = list(decompress_lzw(codewords))
-      lzw_bytes = extract_data_from_lzw_lines(lzw_strings)
-      out_data = decompress_rle(lzw_bytes)
-      offset_after = in_file.tell()
+    for in_offset in potential_offsets:
+        try:
+            in_file.seek(in_offset)
+            codewords = list(iter_codewords(in_file))
+            lzw_strings = list(decompress_lzw(codewords))
+            lzw_bytes = extract_data_from_lzw_lines(lzw_strings)
+            out_data = decompress_rle(lzw_bytes)
+            offset_after = in_file.tell()
 
-      while in_file.read(1) == b'\xFF':
-        pass
-      next_byte = in_file.tell()-1
+            while in_file.read(1) == b"\xFF":
+                pass
+            next_byte = in_file.tell() - 1
 
-      if offset_after - in_offset > 100:
-        print(
-          f'| {in_offset:06x} | {offset_after:06x} | {offset_after-in_offset: 6} bytes '
-          f'| {len(out_data): 6} bytes | {len(out_data) / (offset_after-in_offset):.2f} : 1 '
-          f'| {next_byte - offset_after: 6} bytes |',
-          flush=True
-        )
-    except:
-      pass
+            if offset_after - in_offset > 100:
+                print(
+                    f"| {in_offset:06x} | {offset_after:06x} | {offset_after-in_offset: 6} bytes "
+                    f"| {len(out_data): 6} bytes | {len(out_data) / (offset_after-in_offset):.2f} : 1 "
+                    f"| {next_byte - offset_after: 6} bytes |",
+                    flush=True,
+                )
+        except:
+            pass
 
 
 def main(in_path, out_path, debug_path):
-  in_path, in_offset = parse_path_offset(in_path)
-  out_path, out_offset = parse_path_offset(out_path)
-  in_file = open(in_path, 'rb')
-  if in_offset is not None:
-    in_file.seek(in_offset)
-  else:
-    in_offset = 0
+    in_path, in_offset = parse_path_offset(in_path)
+    out_path, out_offset = parse_path_offset(out_path)
+    in_file = open(in_path, "rb")
+    if in_offset is not None:
+        in_file.seek(in_offset)
+    else:
+        in_offset = 0
 
-  codewords = list(iter_codewords(in_file))
-  offset_after = in_file.tell()
-  print(f'Read {len(codewords)} codewords from {in_offset:06x}-{offset_after:06x} ({offset_after-in_offset} bytes)')
+    codewords = list(iter_codewords(in_file))
+    offset_after = in_file.tell()
+    print(
+        f"Read {len(codewords)} codewords from {in_offset:06x}-{offset_after:06x} ({offset_after-in_offset} bytes)"
+    )
 
-  lzw_strings = list(decompress_lzw(codewords))
-  print(f'Decompressed LZW to {len(lzw_strings)} strings')
-  if debug_path is not None:
-    write_lzw_debug_file(open(debug_path, 'wt'), lzw_strings)
-    print('Wrote debug file')
+    lzw_strings = list(decompress_lzw(codewords))
+    print(f"Decompressed LZW to {len(lzw_strings)} strings")
+    if debug_path is not None:
+        write_lzw_debug_file(open(debug_path, "wt"), lzw_strings)
+        print("Wrote debug file")
 
-  lzw_bytes = extract_data_from_lzw_lines(lzw_strings)
-  print(f'Decompressed LZW to {len(lzw_bytes)} bytes')
-  out_data = decompress_rle(lzw_bytes)
-  print(f'Decompressed RLE to {len(out_data)} bytes')
+    lzw_bytes = extract_data_from_lzw_lines(lzw_strings)
+    print(f"Decompressed LZW to {len(lzw_bytes)} bytes")
+    out_data = decompress_rle(lzw_bytes)
+    print(f"Decompressed RLE to {len(out_data)} bytes")
 
-  if out_offset is not None:
-    out_file = open(out_path, 'w+b')
-    out_file.seek(out_offset)
-  else:
-    out_file = open(out_path, 'wb')
-  out_file.write(out_data)
+    if out_offset is not None:
+        out_file = open(out_path, "w+b")
+        out_file.seek(out_offset)
+    else:
+        out_file = open(out_path, "wb")
+    out_file.write(out_data)
 
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-    'in_path', help='Input file path, optionally suffixed by colon then a byte offset e.g. rom.sfc:0x80ff or rom.sfc:65535 (prefix with 0x for hex)'
-  )
-  parser.add_argument(
-    'out_path', help='Output file path, optionally suffixed by colon then an offset to write a range into an existing file'
-  )
-  parser.add_argument('--debug', nargs='?', help='(Optional) Path to write a debug file listing compressed strings')
-  args = parser.parse_args()
-  main(args.in_path, args.out_path, args.debug)
-
-  # offset = 0x48000 # Avatar, Lord British, etc
-  # offset = 0x28060 # Intro
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "in_path",
+        help="Input file path, optionally suffixed by colon then a byte offset e.g. rom.sfc:0x80ff or rom.sfc:65535 (prefix with 0x for hex)",
+    )
+    parser.add_argument(
+        "out_path",
+        help="Output file path, optionally suffixed by colon then an offset to write a range into an existing file",
+    )
+    parser.add_argument(
+        "--debug",
+        nargs="?",
+        help="(Optional) Path to write a debug file listing compressed strings",
+    )
+    args = parser.parse_args()
+    main(args.in_path, args.out_path, args.debug)
